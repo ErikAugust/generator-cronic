f92eae38e0fabc88a3ed4bfafe2c8a7e581d70d3
Erik August Johnson <erik@erikaugust.com>
2016-11-25 18:59:33 +0000

    Completes basic appliction setup generation

--- generators/app/index.js
+++ generators/app/index.js
@@ -13,15 +13,55 @@ module.exports = yeoman.Base.extend({
     var prompts = [
       {
         type: 'confirm',
+        name: 'eloquent',
+        message: 'Database: Would you like to use Eloquent (ORM)?',
+        default: true
+      },
+      {
+        type: 'confirm',
         name: 'redis',
-        message: 'Would you like to use Redis?',
+        message: 'Cache: Would you like to use Redis?',
         default: true
       },
       {
         type: 'confirm',
         name: 'memcache',
-        message: 'Would you like to use Memcache/Memcached?',
-        default: true
+        message: 'Cache: Would you like to use Memcache / Memcached?',
+        default: false
+      },
+      {
+        type: 'list',
+        name: 'databaseType',
+        message: 'Default Database: Enter type of default database',
+        default: 'mysql',
+        choices: ['mysql', 'postgres', 'sqlsrv', 'sqlite']
+      },
+      {
+        type: 'input',
+        name: 'databaseHost',
+        message: 'Default Database: Enter host address of default database',
+        default: 'localhost'
+      },
+      {
+        type: 'input',
+        name: 'databasePort',
+        message: 'Default Database: Enter port of default database',
+        default: '3306'
+      },
+      {
+        type: 'input',
+        name: 'databaseName',
+        message: 'Default Database: Enter name of default database'
+      },
+      {
+        type: 'input',
+        name: 'databaseUsername',
+        message: 'Default Database: Enter username of default database'
+      },
+      {
+        type: 'input',
+        name: 'databasePassword',
+        message: 'Default Database: Enter password of default database'
       }
     ];
 
@@ -33,15 +73,39 @@ module.exports = yeoman.Base.extend({
 
   writing: function () {
 
+    /**
+     * app.ini
+     */
     this.fs.copyTpl(
       this.templatePath('_app.ini'),
       this.destinationPath('php/config/app.ini'),
       {data: this.props}
     );
 
-    /**this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );**/
+    /**
+     * database.ini
+     */
+    this.fs.copyTpl(
+      this.templatePath('_database.ini'),
+      this.destinationPath('php/config/database.ini'),
+      {data: this.props}
+    );
+
+
+    /**
+     * redis.ini
+     */
+    this.fs.copy(
+      this.templatePath('_redis.ini'),
+      this.destinationPath('php/config/redis.ini')
+    );
+
+    /**
+     * memcache.ini
+     */
+    this.fs.copy(
+      this.templatePath('_memcache.ini'),
+      this.destinationPath('php/config/memcache.ini')
+    );
   }
 });
